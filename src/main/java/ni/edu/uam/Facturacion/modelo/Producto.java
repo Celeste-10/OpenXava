c8db39697bf1566c6c2a82ca7871737a4a2a603f
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-05-29 16:14:06 -0600

    feat: Modelo basico del dominio -Parte 1 completado

--- src/main/java/ni/edu/uam/Facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Producto.java
@@ -2,11 +2,10 @@ package ni.edu.uam.Facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.openxava.annotations.Required;
+import org.openxava.annotations.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -21,5 +20,19 @@ public class Producto {
     @Required
     String descripcion;
 
+    @ManyToOne
+            (fetch = FetchType.LAZY, optional = true)
+    @DescriptionsList
+    Categoria categoria;
+
+    @Money
+    BigDecimal precio;
+
+    @Files
+    @Column(length = 32)
+    String fotos;
+
+    @TextArea
+    String observaciones;
 }
 
