00de0d31776940f305fa6f260cd2b5725ca16be4
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-05-29 18:56:16 -0600

    feat: Se refino la interfaz de usuario

--- src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
@@ -13,6 +13,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 
 @Entity @Getter @Setter
+@View(members = "anyo, numero, fecha;" + "cliente;" + "detalles;" + "observaciones")
 public class Factura {
     @Id
     @GeneratedValue(generator = "system-uuid")
@@ -34,6 +35,7 @@ public class Factura {
     LocalDate fecha;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false )
+    @ReferenceView("Simple")
     Cliente cliente;
 
     @ElementCollection
